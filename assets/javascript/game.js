ea513520b39a710302642463e68ef287c1cefbb8
Robert Prine <robertprine2@yahoo.com>
2016-05-12 05:14:29 -0400

    Added files, images, and basic html skeleton

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Waits until html is finished loading then starts JS
+
+$(document).ready(function(){
+
+	//game object to contain variables and methods
+
+	var game = {
+
+		
+
+	} // Ends game object
+
+	
+
+}); // Ends document.ready function
